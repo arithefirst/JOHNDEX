625aee640a81baea9f0755e79ba5af2e9f7b580e
April Hall <ari@arithefirst.com>
2025-03-20 17:05:38 -0400

    build: Init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import '../app.css';
+  let { children } = $props();
+</script>
+
+<main class="font-primary text-text min-h-screen bg-[#081216]">
+  {@render children()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="mx-auto w-fit">
+  <h1 class="pt-10 text-center text-7xl">JOHNDEX</h1>
+  <hr class="w-full border" />
+</div>
