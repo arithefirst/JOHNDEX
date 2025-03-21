f2a5d70ac768547a3f4f41fe1fd6933cd9de7e2f
April Hall <ari@arithefirst.com>
2025-03-20 23:28:28 -0400

    feat: DARKNESS in the clerk provider

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import '../app.css';
   import { ClerkProvider } from 'svelte-clerk';
+  import { dark } from '@clerk/themes';
   let { children } = $props();
 </script>
 
-<ClerkProvider>
+<ClerkProvider
+  appearance={{
+    baseTheme: dark,
+  }}
+>
   {@render children()}
 </ClerkProvider>
