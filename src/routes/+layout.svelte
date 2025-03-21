7aef27e6a8dcd45ab48ff8be7f6be88ace116664
April Hall <ari@arithefirst.com>
2025-03-21 11:10:12 -0400

    feat: Enable analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-  import '../app.css';
-  import { ClerkProvider } from 'svelte-clerk';
+  import { dev } from '$app/environment';
   import { dark } from '@clerk/themes';
+  import { injectAnalytics } from '@vercel/analytics/sveltekit';
+  import { ClerkProvider } from 'svelte-clerk';
+  import '../app.css';
   let { children } = $props();
+
+  injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <ClerkProvider
