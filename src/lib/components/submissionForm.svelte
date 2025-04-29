54227409b834a755d7d79e2a8adebee9390c0eb7
April Hall <ari@arithefirst.com>
2025-04-29 13:11:42 -0400

    fix: Submit form changes

--- src/lib/components/submissionForm.svelte
+++ src/lib/components/submissionForm.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
   import * as Dialog from '$lib/components/ui/dialog/index.js';
-  import { enhance } from '$app/forms';
+  import { Input } from '$lib/components/ui/input/index.js';
+  import { Label } from '$lib/components/ui/label/index.js';
   import { superForm } from 'sveltekit-superforms';
 
   let { data } = $props();
-  const { form, errors } = superForm(data.form);
+
+  let open = $state(false);
+
+  const { form, errors, enhance } = superForm(data.form, {
+    onResult: (e) => (open = false),
+  });
 </script>
 
-<Dialog.Root>
+<Dialog.Root bind:open>
   <Dialog.Trigger class="text-text font-primary bg-primary h-fit cursor-pointer rounded-md px-2"
     >Add new John</Dialog.Trigger
   >
@@ -16,40 +22,53 @@
       <Dialog.Header>
         <Dialog.Title>Add new John</Dialog.Title>
       </Dialog.Header>
-      <p>
-        John
-        <input
-          class="border-text w-16 border-b outline-0"
-          type="text"
-          name="name"
-          id="name"
-          aria-invalid={$errors.name ? 'true' : undefined}
-          bind:value={$form.name}
-        />
 
-        , inventor of
-        <input
-          class="border-text w-16 border-b outline-0"
-          type="text"
-          name="item"
-          id="item"
-          aria-invalid={$errors.item ? 'true' : undefined}
-          bind:value={$form.item}
-        />
-      </p>
-      <Dialog.Footer class={$errors.name || $errors.item ? 'mt-3' : ''}>
-        <div class="mr-auto flex w-full flex-col">
-          {#if $errors.name}<span class="w-full text-center text-red-500 md:w-auto md:text-left"
-              >Name: {$errors.name}</span
-            >{/if}
-          {#if $errors.item}<span class="w-full text-center text-red-500 md:w-auto md:text-left"
-              >Invention: {$errors.item}</span
-            >{/if}
+      <div class="flex flex-col items-center justify-center gap-2">
+        <!-- Name feild -->
+        <div class="w-11/12">
+          <Label for="name">Name</Label>
+          <div
+            class="border-input flex h-9 w-full items-center justify-start gap-1 rounded-md border bg-transparent px-3 py-1 text-base shadow-sm transition-colors md:text-sm"
+          >
+            <span class="text-muted-foreground">John</span>
+            <input
+              class="flex-grow ring-0 outline-0"
+              type="text"
+              name="name"
+              id="name"
+              aria-invalid={$errors.name ? 'true' : undefined}
+              bind:value={$form.name}
+            />
+          </div>
         </div>
-        <button type="submit" class="text-text font-primary bg-primary h-fit cursor-pointer rounded-md px-2 py-1">
-          Submit
-        </button>
-      </Dialog.Footer>
+        <!-- Invention Field -->
+        <div class="w-11/12">
+          <Label for="item">Invention</Label>
+          <Input
+            type="text"
+            name="item"
+            id="item"
+            aria-invalid={$errors.item ? 'true' : undefined}
+            bind:value={$form.item}
+          />
+        </div>
+        <Dialog.Footer class={$errors.name || $errors.item ? 'mt-3' : ''}>
+          <div class="mr-auto flex w-full flex-col">
+            {#if $errors.name}<span class="w-full text-center text-red-500 md:w-auto md:text-left"
+                >Name: {$errors.name}</span
+              >{/if}
+            {#if $errors.item}<span class="w-full text-center text-red-500 md:w-auto md:text-left"
+                >Invention: {$errors.item}</span
+              >{/if}
+          </div>
+          <button
+            type="submit"
+            class="text-text font-primary bg-primary h-fit cursor-pointer rounded-md px-2 py-1"
+          >
+            Submit
+          </button>
+        </Dialog.Footer>
+      </div>
     </form>
   </Dialog.Content>
 </Dialog.Root>
